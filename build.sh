10943906c0aaf27e7a7201f215077b537ff42b50
woroko <29544613+woroko@users.noreply.github.com>
2018-05-12 01:33:48 +0300

    android build

---
+++ build.sh
@@ -0,0 +1,3 @@
+p4a apk --requirements=python2,kivy,numpy,audiostream --private . --package=org.example.myapp --name "RXTX" --version 0.1 \
+--bootstrap=sdl2 --java-build-tool gradle --debug --sdk-dir ~/Android/Sdk --ndk-dir ~/Android/Sdk/ndk-bundle --android-api 19 \
+--arch=armeabi-v7a --ndk-version r17
